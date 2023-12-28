1f2209ec09e6c28ee10c6fb38f22983bf23bd1cf
greenArrow1 <chittrang.m@gmail.com>
2023-12-28 19:51:25 +0530

    added database and charts

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,15 @@
+import { sql } from '@vercel/postgres';
+ 
+async function loaddata(){
+    const likes = 100;
+    const { rows, fields } =
+      await sql`SELECT invoices.amount, customers.name
+      FROM invoices
+      JOIN customers ON invoices.customer_id = customers.id
+      WHERE invoices.amount = 666`;
+      return rows;
+}
 export default function Page(){
+    console.log('data',loaddata());
 return <p>Customers Page</p>
 }
